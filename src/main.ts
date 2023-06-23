29118312097a97da6dec0e4b23f5e8e6f4712b4c
ahiipsa <ahiipsa@gmail.com>
2023-06-24 01:07:03 +0400

    migrate to grammy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {initTelegramClient} from "./telegram";
+import {initTelegramBot} from "./telegramBot";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await initTelegramClient()
+  const bot = initTelegramBot();
+  await bot.start({onStart: (param) => {
+    console.log('### param', param);
+    }});
   await app.listen(8080);
 }
 bootstrap();
---
+++ src/telegramBot.ts
@@ -0,0 +1,157 @@
+import {Bot, Context, InputFile, MemorySessionStorage, session, SessionFlavor} from "grammy";
+import { config } from './config'
+import * as path from "path";
+import {createQRCode} from "./qrcode";
+import {SDClient} from "./sdClient/SDClient";
+
+interface SessionData {
+    qrMargin: number
+}
+
+type BotContext = Context & SessionFlavor<SessionData>;
+
+const sdClient = new SDClient();
+
+
+const sendQrCodeImg2Img = async (url: string, prompt: string, ctx: BotContext) => {
+  if (!url) {
+    await ctx.reply("command /qr should contain an url");
+    return;
+  }
+
+  try {
+    const qrImageBuffer = await createQRCode({url: url, margin: ctx.session.qrMargin});
+    const imgBuffer = await sdClient.img2img(qrImageBuffer.toString('base64'), prompt);
+
+    if (!imgBuffer) {
+      // TODO: send feedback
+      return;
+    }
+    const command = `/qr ${url} ${prompt}`
+
+    await ctx.replyWithPhoto(new InputFile(imgBuffer, 'qr_code.png'), {
+      caption: command,
+    })
+  } catch (ex) {
+    ctx.reply('internal error')
+  }
+}
+
+export const initTelegramBot = (): Bot => {
+
+  const bot = new Bot<BotContext>(config.telegramToken);
+
+  function createInitialSessionData(): SessionData {
+    return {  qrMargin: 1 };
+  }
+  bot.use(session({ initial: createInitialSessionData, storage: new MemorySessionStorage() }));
+
+  bot.command("start", (ctx) => {
+    (async () => {
+      await ctx.reply("Send a message with the \"qr\" command and your prompts: \n/qr LINK PROMPTS." +
+        "\n\n" +
+        "Example:\n/qr h.country/ai astronaut, sky, colorful", {disable_web_page_preview: true});
+      const filePath = path.join(__dirname, '../files/qrcodes/demo_astronaut_qr.png');
+      // const imgBuffer = fs.readFileSync(filePath);
+
+      await ctx.replyWithPhoto(new InputFile(filePath), {});
+    })();
+  })
+
+  bot.command("qrMargin", (ctx) => {
+    const margin = parseInt(ctx.match, 10);
+
+    console.log('### ctx.match', ctx.match);
+    console.log('### margin', margin);
+    if (!isNaN(margin)) {
+      console.log('### set margin');
+      ctx.session.qrMargin = margin;
+    }
+    ctx.reply('qrMargin: ' + ctx.session.qrMargin)
+  });
+
+  bot.command('qr2', (ctx) => {
+    (async () => {
+      if (!ctx.message || !ctx.message.text) {
+        // TODO: send feedback
+        ctx.reply("Please send /qr command")
+        return;
+      }
+
+
+      ctx.reply('Wait a minute...');
+
+      const [command, url, ...prompts] = ctx.message.text.split(' ');
+
+      if (!url) {
+        await ctx.reply("command /qr should contain an url");
+        return;
+      }
+
+      try {
+        const qrImageBuffer = await createQRCode({url: url, margin: ctx.session.qrMargin});
+        const imgBuffer = await sdClient.text2img(qrImageBuffer.toString('base64'), prompts.join(' '));
+
+        if (!imgBuffer) {
+          // TODO: send feedback
+          return;
+        }
+
+        const command = `/qr2 ${url} ${prompts.join(' ')}`
+
+        await ctx.replyWithPhoto(new InputFile(imgBuffer, 'qr_code.png'), {
+          caption: command,
+        })
+
+
+      } catch (ex) {
+        ctx.reply('internal error')
+      }
+    })()
+  })
+
+  bot.command('help', (ctx) => {
+    ctx.reply("/start - start the bot\n" +
+      "/qr LINK PROMPTS\n" +
+      "Example:\n" +
+      "/qr h.country/ai astronaut, sky, colorful\n" +
+      "/qr2 LINK PROMPTS" +
+      "Example:\n" +
+      "/qr2 h.country/ai astronaut, sky, colorful\n" +
+      "/qrMargin - set qr margin", {disable_web_page_preview: true})
+  })
+
+  bot.command('qr', (ctx) => {
+
+    (async () => {
+
+      if (!ctx.message || !ctx.message.text) {
+        // TODO: send feedback
+        ctx.reply("Please send /qr command")
+        return;
+      }
+
+      ctx.reply('Wait a minute...');
+
+      console.log('### ctx.message.text', ctx.message.text);
+      const [command, url, ...prompts] = ctx.message.text.split(' ');
+
+      if (!url) {
+        await ctx.reply("command /qr should contain an url");
+        return;
+      }
+
+      await sendQrCodeImg2Img(url, prompts.join(' '), ctx)
+
+
+    })()
+  });
+
+  bot.on("message", async (ctx) => {
+    ctx.reply("Please send /qr command");
+  })
+
+  return bot;
+}
+
+
