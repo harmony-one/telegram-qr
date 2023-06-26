e2915028ddaf64d5084c9778b3a42532fd703f5c
ahiipsa <ahiipsa@gmail.com>
2023-06-26 17:52:11 +0400

    removed host

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,5 @@ export const config = {
   telegramToken: process.env.TELEGRAM_TOKEN || '',
   telegramApiId: parseInt(process.env.TELEGRAM_API_ID || '0', 10) || 0,
   telegramApiHash: process.env.TELEGRAM_API_HASH || '',
+  sdHost: process.env.SD_HOST || '',
 } as const;
